1d07d95502b7583b67a978cad202728eeaea68d3
rgbpokka <jeremyyeozhichen99@gmail.com>
2021-12-15 21:50:26 +0800

    Add basic logic for joining and leaving queues

--- bot/api/service/main.py
+++ bot/api/service/main.py
@@ -1,1 +1,23 @@
 #Start of main.py
+import queue
+
+class EventManager: 
+    def __init__(self):
+        self.events = []
+
+    def addEvent(self, event):
+        self.events.append(event)
+
+    # need to handle exception
+    def removeEvent(self, event):
+        self.events.remove(event)
+
+    def addUserToEvent(self, event, user):
+        event.addToEvent(user)
+
+    def removeUserFromEvent(self, event, user):
+        event.removeFromEvent(user)
+
+    
+
+
